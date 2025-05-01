99d07f50de3c05c92f927540923d7af59450f622
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-05-01 22:59:12 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NavigationBarBottom from "./navigation-bar-bottom";
+import NavigationBarBottom from "./navigation-bar-bottom.tsx";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
