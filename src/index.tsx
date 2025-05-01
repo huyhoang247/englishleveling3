bf308529c582194b631ab4bbe909f8adde4837df
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-05-01 21:22:17 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,4 @@ import Profile from './profile.tsx';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root not found');
-createRoot(container).render(<VerticalFlashcardGallery />);
+createRoot(container).render(<NavigationBarBottom />);
