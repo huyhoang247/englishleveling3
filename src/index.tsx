a4a54f9453c004893eb727a8175e75bebf09aee2
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-05-02 02:10:31 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import VerticalFlashcardGallery from './VerticalFlashcardGallery.tsx';
-import NavigationBarBottom from './navigation-bar-bottom.tsx';
+import Navigationbarbottom from './home.tsx';
 import Profile from './profile.tsx';
 
 const container = document.getElementById('root');
