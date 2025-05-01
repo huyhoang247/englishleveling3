a09b6bbf5a5db0f984cbe8bbddb3c2509e59b6f4
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-05-01 22:58:09 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import VerticalFlashcardGallery from './VerticalFlashcardGallery.tsx';
-import NavigationBarBottom from './navigation-bar-bottom.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NavigationBarBottom from "./navigation-bar-bottom";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <div className="app-container" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <div style={{ flex: 1, overflow: 'auto' }}>
-        <VerticalFlashcardGallery />
-      </div>
-      <NavigationBarBottom />
-    </div>
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
 );
+root.render(<NavigationBarBottom />);
