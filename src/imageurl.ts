c71c76d9c3c04029670cf514be93528009695d13
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-05-07 23:37:16 +0700

    Create imageurl.ts

---
+++ src/imageurl.ts
@@ -0,0 +1,16 @@
+// imageUrls.ts
+
+// Danh sách URL ảnh cho phong cách Mặc định (Default)
+export const defaultImageUrls: string[] = [
+  "https://images.pixieset.com/59415508/7a1f30f4b95115d4cab5265ad0c40782-xxlarge.png",
+  "https://images.pixieset.com/59415508/1d0b68793afa4a11abd5eb35d762c5ba-xxlarge.png",
+  "https://images.pixieset.com/59415508/93649b3d0569a29a6ad60cda73df8c40-xxlarge.png",
+  "https://images.pixieset.com/59415508/340229e596d6c1c4b6e6161cf88cf4aa-xxlarge.png",
+  "https://images.pixieset.com/59415508/cab96473a94e603273c6505bea2bdb3f-xxlarge.png",
+  // Thêm các URL ảnh mặc định khác tại đây
+];
+
+// Bạn có thể thêm các mảng URL ảnh cho các phong cách khác tại đây nếu muốn
+// export const animeImageUrls: string[] = [ ... ];
+// export const comicImageUrls: string[] = [ ... ];
+// export const realisticImageUrls: string[] = [ ... ];
