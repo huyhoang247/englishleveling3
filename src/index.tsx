8be74dee36b6739077f9b02939737169dab298ec
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-05-08 15:39:17 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import NavigationBarBottom from './navigation-bar-bottom.tsx';
 import Story from './VerticalFlashcardGallery.tsx'; // Import VerticalFlashcardGallery
 import Profile from './profile.tsx';
 import Quiz from './stats/reset-points.tsx';
+import Auth from './auth.ts';    // ← import component Auth mới
+
 
 // Define the possible tab types
 type TabType = 'home' | 'profile' | 'story' | 'quiz';
