8a44581dafdc1ca8010071b1fbcaf485cfea321a
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-06-08 07:56:35 +0700

    Update blacksmith.tsx

--- src/blacksmith.tsx
+++ src/blacksmith.tsx
@@ -893,7 +893,8 @@ const Blacksmith = ({ onClose }) => { // Accept onClose prop
         </div>
         {/* ----- END: FINALIZED TOP BAR ----- */}
 
-        <div className="grid lg:grid-cols-2 gap-y-2 gap-x-8 flex-grow overflow-y-auto hide-scrollbar mt-4"> {/* MODIFIED: Reduced vertical gap */}
+        {/* MODIFIED: Added min-h-0 to ensure flex container can shrink and enable scrolling */}
+        <div className="grid lg:grid-cols-2 gap-y-2 gap-x-8 flex-grow overflow-y-auto hide-scrollbar mt-4 min-h-0">
           
           {/* ----- START: SIMPLIFIED UPGRADE TAB ----- */}
           {activeTab === 'upgrade' && (
