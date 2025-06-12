1b68885a89efb9a7b5ba82753ddc1ac5a2154f8b
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-06-12 14:18:42 +0700

    Update player-inventory-data.ts

--- src/inventory/player-inventory-data.ts
+++ src/inventory/player-inventory-data.ts
@@ -60,7 +60,8 @@ export const playerInventoryData: PlayerItem[] = [
     { instanceId: 1044, id: 55, quantity: 1 },
     { instanceId: 1045, id: 56, quantity: 1 },
     { instanceId: 1046, id: 57, quantity: 1 },
-    { instanceId: 1047, id: 58, quantity: 1 }
+    { instanceId: 1047, id: 58, quantity: 1 },
+    { instanceId: 1048, id: 59, quantity: 1 }
 ];
 
 // --- END OF FILE src/data/player-inventory-data.ts ---
