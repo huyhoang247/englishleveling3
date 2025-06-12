a6beb1306ef7e81a26c94fffbaf227d8e7eead98
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-06-12 22:22:20 +0700

    Create player-equipment-data.ts

---
+++ src/player-equipment-data.ts
@@ -0,0 +1,10 @@
+// --- START OF FILE inventory/player-equipment-data.ts ---
+export const playerEquipmentData = {
+  weapon: 'longsword-001', // instanceId của vật phẩm trong player-inventory-data
+  armor: 'leather-armor-001',
+  // helmet: null, // Ví dụ: chưa trang bị mũ
+  // boots: null,
+  // gloves: null,
+  // ring: 'ring-of-power-001'
+};
+// --- END OF FILE inventory/player-equipment-data.ts ---
