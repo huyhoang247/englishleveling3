b94f9cf56eb8dab137872145ddfe696088a54b7f
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-07-08 23:51:49 +0700

    Update flashcard.tsx

--- src/story/flashcard.tsx
+++ src/story/flashcard.tsx
@@ -155,7 +155,7 @@ const FlashcardDetailModal: React.FC<FlashcardDetailModalProps> = ({
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-500 dark:text-blue-400 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                 </svg>
-                <span className="font-sans text-lg font-bold uppercase tracking-widest text-blue-600 dark:text-blue-400">
+                <span className="font-sans text-base font-bold uppercase tracking-widest text-blue-600 dark:text-blue-400">
                   {wordToFind}
                 </span>
               </div>
