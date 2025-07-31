6112ae0ec577c386042c2f20b38dd31327f5d8a5
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-07-31 14:51:30 +0700

    Create phrases.ts

---
+++ src/phrases.ts
@@ -0,0 +1,26 @@
+// --- START OF FILE: phrases.ts ---
+
+export const phrases: string[] = [
+  "Primary source",
+  "Reliable source",
+  "Energy source",
+  "Source of information",
+  "Source code",
+  "Source material",
+  "Source of income",
+  "Source document",
+  "Source language",
+  "Source data",
+  "Health insurance",
+  "Life insurance",
+  "Car insurance",
+  "Travel insurance",
+  "Insurance policy",
+  "Home insurance",
+  "Insurance premium",
+  "Insurance claim",
+  "Insurance coverage",
+  "Insurance company"
+];
+
+// --- END OF FILE: phrases.ts ---
