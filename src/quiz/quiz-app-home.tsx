e6df8c8dd38000036c9f4da030f4ac5b1898b578
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-08-08 11:34:37 +0700

    Update quiz-app-home.tsx

--- src/quiz/quiz-app-home.tsx
+++ src/quiz/quiz-app-home.tsx
@@ -457,7 +457,6 @@ const PracticeCard = memo(({ practiceNumber, details, progress, onPracticeSelect
                             </div>
                         )
                     )}
-                    <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 text-gray-400 ${colors.arrow} transition-colors`} fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" /></svg>
                 </div>
             </div>
             <div className="border-t border-gray-200 mt-3 pt-3 flex justify-between items-center">
