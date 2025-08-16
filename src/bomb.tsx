0de6f28e083672143b408481cd13ff5d44b1a500
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-08-16 11:18:13 +0700

    Update bomb.tsx

--- src/bomb.tsx
+++ src/bomb.tsx
@@ -1,7 +1,7 @@
 // --- START OF FILE bomb.tsx (5).txt ---
 
 import React, { useState, memo, useCallback } from 'react';
-import CoinDisplay from './coin-display.tsx';
+import CoinDisplay from './ui/display/coin-display.tsx';
 
 // --- Các component Icon SVG & IMG (Không thay đổi) ---
 const XIcon = ({ size = 24, color = 'currentColor', className = '', ...props }) => ( <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`lucide-icon ${className}`} {...props}> <line x1="18" y1="6" x2="6" y2="18" /> <line x1="6" y1="6" x2="18" y2="18" /> </svg> );
