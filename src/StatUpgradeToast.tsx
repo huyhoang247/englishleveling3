9a72b9ad939258de4450aeb4ad6df06b0f43f969
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-08-19 00:46:40 +0700

    Update StatUpgradeToast.tsx

--- src/StatUpgradeToast.tsx
+++ src/StatUpgradeToast.tsx
@@ -14,7 +14,7 @@ interface StatUpgradeToastProps {
 const formatBonus = (num: number) => {
     if (num < 1000) return num.toString();
     if (num < 1000000) return `${(num / 1000).toFixed(1).replace('.0', '')}K`;
-    return `+${(num / 1000000).toFixed(1).replace('.0', '')}M`;
+    return `${(num / 1000000).toFixed(1).replace('.0', '')}M`;
 };
 
 // Đặt keyframes animation ngay trong file cho dễ quản lý
