2b564a491a2d14ffd1a054629f08cced5a35b967
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-08-19 21:54:07 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import Home from './background-game.tsx'; // Component chính của màn hình 'home'
 import NavigationBarBottom from './navigation-bar-bottom.tsx';
 import Story from './VerticalFlashcardGallery.tsx';
-import Profile from './building.tsx';
+import Profile from './candy.tsx';
 import QuizAppHome from './courses/course-ui.tsx';
 import GameBrowser from './game.tsx';
 import AuthComponent from './auth.js';
