c3342eb70f73462a462ccdfec85bbf3b1b12e399
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-08-30 12:23:16 +0700

    Update flashcard.tsx

--- src/story/flashcard.tsx
+++ src/story/flashcard.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import BackButton from '../footer-back.tsx';
 import { ExampleSentence, Flashcard as CoreFlashcard } from './flashcard-data.ts';
-import { generateAudioUrlsForWord } from '../audio-quiz-generator.ts'; // <-- THÊM IMPORT MỚI
+import { generateAudioUrlsForWord } from '../voca-data/audio-quiz-generator.ts'; // <-- THÊM IMPORT MỚI
 
 // Đổi tên interface để tránh xung đột với tên component, mặc dù vẫn dùng chung cấu trúc từ file data
 interface FlashcardData extends CoreFlashcard {}
