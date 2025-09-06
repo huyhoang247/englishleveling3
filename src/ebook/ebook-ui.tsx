a6dfcb18b7cfcb57f713d0f1b1d0035dea4ac768
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-09-06 22:16:51 +0700

    Update ebook-ui.tsx

--- src/ebook/ebook-ui.tsx
+++ src/ebook/ebook-ui.tsx
@@ -4,9 +4,9 @@ import React, { useMemo } from 'react';
 import { EbookProvider, useEbook, Book, Vocabulary, PhraseSentence, Flashcard } from './EbookContext.tsx';
 
 // --- COMPONENT & MODAL IMPORTS ---
-import FlashcardDetailModal from './story/flashcard.tsx';
-import AddToPlaylistModal from './AddToPlaylistModal.tsx';
-import PhraseDetailModal from './PhraseDetailModal.tsx';
+import FlashcardDetailModal from '../story/flashcard.tsx';
+import AddToPlaylistModal from '../AddToPlaylistModal.tsx';
+import PhraseDetailModal from '../PhraseDetailModal.tsx';
 
 // --- ICONS (Copied from original file for self-containment) ---
 const PlayIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" /></svg>);
