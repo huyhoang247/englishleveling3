057881f973e522a2e70f143396717c7d9141e348
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-11 13:28:20 +0700

    Create index.ts

---
+++ src/ebook/ebooks/index.ts
@@ -0,0 +1,11 @@
+// src/data/books/index.ts
+import { Book } from '../types.ts';
+
+import { book1Data } from './book1.ts';
+
+
+// Tổng hợp tất cả dữ liệu sách vào một mảng duy nhất
+export const allBooks: Book[] = [
+  book1Data,
+  
+];
