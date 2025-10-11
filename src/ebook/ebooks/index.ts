5241e3c0f0055e26eaae2a147cfe01a1526c8d6c
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-11 13:48:44 +0700

    Update index.ts

--- src/ebook/ebooks/index.ts
+++ src/ebook/ebooks/index.ts
@@ -2,10 +2,13 @@
 import { Book } from '../types.ts';
 
 import { book1Data } from './book1.ts';
+import { book2Data } from './book12.ts';
+
 
 
 // Tổng hợp tất cả dữ liệu sách vào một mảng duy nhất
 export const allBooks: Book[] = [
   book1Data,
+  book2Data,
   
 ];
