a634d3b6d9073e0b4d2df57a8401f922afe4250c
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-12 15:42:04 +0700

    Update book4.ts

--- src/ebook/ebooks/book4.ts
+++ src/ebook/ebooks/book4.ts
@@ -1,4 +1,3 @@
-
 import { Book } from '../types';
 
 export const book4Data: Book = {
@@ -9,284 +8,82 @@ export const book4Data: Book = {
   coverImageUrl: 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/refs/heads/main/src/audio-ted/image/002.jpg',
   audioUrls: {
     'Tim Urban': 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/main/src/audio-ted/audio/origin-voice/002.mp3',
-  
   },
   content: `
 
+If you know an older left-handed person, chances are they had to learn to write or eat with their right hand.
+And in many parts of the world, it's still common practice to force children to use their "proper" hand.
+Even the word for "right" also means "correct" or "good", not just in English but many other languages too.
+But if being left-handed is so wrong, then why does it happen in the first place?
+Today, about one tenth of the world's population are left-handed.
+Archaeological evidence shows that it's been that way for as long as five hundred thousand years, with about 10% of human remains showing the associated differences in arm length and bone density, and some ancient tools and artifacts showing evidence of left hand use.
+And despite what many may think, handedness is not a choice.
+It can be predicted even before birth based on the fetuses' position in the womb.
+So if handedness is inborn, does that mean it's genetic?
+Well, yes and no.
+Identical twins who have the same genes can have different dominant hands.
+In fact, this happens as often as it does with any other sibling pair.
+But the chances of being right or left-handed are determined by the handedness of your parents in surprisingly consistent ratios.
+If your father was left-handed but your mother was right-handed, you have a 17% chance of being born left-handed, while two righties will have a left-handed child only 10% of the time.
+Handedness seems to be determined by a roll of the dice, but the odds are set by your genes.
+All of this implies there's a reason that evolution has produced this small proportion of lefties and maintained it over the course of millennia.
+And while there have been several theories attempting to explain why handedness exists in the first place or why most people are right-handed, a recent mathematical model suggests that the actual ratio reflects a balance between competitive and cooperative pressures on human evolution.
+The benefits of being left-handed are clearest in activities involving an opponent, like combat or competitive sports.
+For example, about 50% of top hitters in baseball have been left-handed.
+Why?
+Think of it as a surprise advantage.
+Because lefties are a minority to begin with, both right-handed and left-handed competitors will spend most of their time countering and practicing against righties.
+So when the two faced each other, the left-hander will be better prepared against this right-handed opponent, while the righty will be thrown off.
+This fighting hypothesis, where an imbalance in the population results in an advantage for left-handed fighters or athletes, is an example of negative frequency-dependent selection.
+But according to the principles of evolution, groups that have a relative advantage tend to grow until that advantage disappears.
+If people were only fighting and competing throughout human evolution, natural selection would lead to more lefties being the ones that made it, until there were so many of them that it was no longer a rare asset.
+So, in a purely competitive world, 50% of the population would be left-handed.
+But human evolution has been shaped by cooperation as well as competition.
+And cooperative pressure pushes handedness distribution in the opposite direction.
+In golf, where performance doesn't depend on the opponent, only 4% of top players are left-handed, an example of the wider phenomenon of tool sharing.
+Just as young potential golfers can more easily find a set of right-handed clubs, many of the important instruments that have shaped society were designed for the right-handed majority.
+Because lefties are worse at using these tools and suffer from higher accident rates, they would be less successful in a purely cooperative world, eventually disappearing from the population.
+So by correctly predicting the distribution of left-handed people in the general population, as well as matching data from various sports, the model indicates that the persistence of lefties as a small but stable minority reflects an equilibrium that comes from competitive and cooperative effects playing out simultaneously over time.
+And the most intriguing thing is what the numbers can tell us about various populations.
+From the skewed distribution of pawedness in cooperative animals to the slightly larger percentage of lefties in competitive hunter-gatherer societies, we may even find that the answers to some puzzles of early human evolution are already in our hands.
 [Music]
-if you know an older left-handed person
-chances are they had to learn to write
-or eat with their right hand and in many
-parts of the world it's still common
-practice to force children to use their
-proper hand even the word for right also
-means correct or good not just in
-English but many other languages too but
-if being left-handed is so wrong then
-why does it happen in the first place
-today about one tenth of the world's
-population are left-handed
-archaeological evidence shows that it's
-been that way for as long as five
-hundred thousand years with about 10% of
-human remains
-showing the associated differences in
-arm length and bone density and some
-ancient tools and artifacts showing
-evidence of left hand use and despite
-what many may think handedness is not a
-choice it can be predicted even before
-birth based on the fetuses position in
-the womb so if handedness is inborn does
-that mean it's genetic well yes and no
-identical twins who have the same genes
-can have different dominant hands in
-fact this happens as often as it does
-with any other sibling pair but the
-chances of being right or left-handed
-are determined by the handedness of your
-parents in surprisingly consistent
-ratios if your father was left-handed
-but your mother was right-handed you
-have a 17% chance of being born
-left-handed while to righties will have
-a left-handed child only 10% of the time
-handedness seems to be determined by a
-roll of the dice but the odds are set by
-your genes all of this implies there's a
-reason that evolution has produced this
-small proportion of lefties and
-maintained it over the course of
-millennia and while there have been
-several theories attempting to explain
-why handedness exists in the first place
-or why most people are right-handed a
-recent mathematical model suggests that
-the actual ratio reflects a balance
-between competitive and cooperative
-pressures on human evolution
-the benefits of being left-handed are
-clearest in activities involving an
-opponent like combat or competitive
-sports for example about 50% of top
-hitters in baseball have been
-left-handed why think of it as a
-surprise advantage because lefties are a
-minority to begin with both right-handed
-and left-handed competitors will spend
-most of their time in countering and
-practicing against righties so when the
-two faced each other the left-hander
-will be better prepared against this
-right-handed opponent while the righty
-will be thrown off this fighting
-hypothesis where an imbalance in the
-population results in an advantage for
-left-handed fighters or athletes is an
-example of negative frequency dependent
-selection but according to the
-principles of evolution groups that have
-a relative advantage tend to grow until
-that advantage disappears if people were
-only fighting and competing throughout
-human evolution natural selection would
-lead to more lefties being the ones that
-made it until there were so many of them
-that it was no longer a rare asset so in
-a purely competitive world 50% of the
-population would be left-handed but
-human evolution has been shaped by
-cooperation as well as competition and
-cooperative pressure pushes handedness
-distribution in the opposite direction
-in golf where performance doesn't depend
-on the opponent only 4% of top players
-are left-handed an example of the wider
-phenomenon of tool sharing just as young
-potential golfers can more easily find a
-set of right-handed clubs many of the
-important instruments that have shaped
-society were designed for the
-right-handed majority because lefties
-are worse at using these tools and
-suffer from higher accident rates they
-would be less successful in a purely
-cooperative world eventually
-disappearing from the population so by
-correctly predicting the distribution of
-left-handed people in the general
-population as well as matching data from
-various sports the model indicates that
-the persistence of lefties as a small
-but stable minority reflects an
-equilibrium that comes from competitive
-and cooperative effects playing out
-simultaneously over time and the most
-intriguing thing is what the numbers can
-tell us about various populations from
-the skewed distribution of podna s' in
-cooperative animals to the slightly
-larger percentage of lefties in
-competitive hunter-gatherer societies we
-may even find that the answers to some
-puzzles of early human evolution are
-already in our hands
-[Music]
-
-
   `,
   contentVi: `
 
-Translator: Ngoc-Anh Nguyen
-Reviewer: Ruby Phan
-Nếu bạn quen một người lớn tuổi
-thuận tay trái,
-rất có thể họ đã phải học viết
-và ăn bằng tay phải.
-Ở nhiều nơi trên thế giới,
-đó là điều bình thường khi bắt trẻ em
-phải sử dụng 'đúng" tay.
-Ngay cả từ "phải"
-cũng có nghĩa là "đúng", hay "tốt",
-không chỉ trong tiếng Anh,
-mà còn trong nhiều ngôn ngữ khác.
-Nhưng nếu thuận tay trái là sai,
-vậy tại sao ngay từ đầu nó lại xuất hiện ?
-Ngày nay,
-1/10 dân số thế giới thuận tay trái.
-Các chứng tích khảo cổ
-cho thấy điều này đã có sẵn
-từ 500.000 năm trước,
-với khoảng 10% hài cốt để lại
-cho thấy những khác biệt
-về độ dài cánh tay và mật độ xương,
-Và các công cụ cổ đại và đồ thủ công
-thể hiện bằng chứng về việc thuận tay trái.
-Và bất kể bạn nghĩ như thế nào,
-bạn không thể lựa chọn tay thuận
-Nó có thể được dự đoán trước khi sinh,
-dựa trên tư thế bào thai trong bụng mẹ.
-Vậy nếu tay thuận là bẩm sinh
-thì liệu nó có di truyền?
+Nếu bạn quen một người lớn tuổi thuận tay trái, rất có thể họ đã phải học viết và ăn bằng tay phải.
+Ở nhiều nơi trên thế giới, đó là điều bình thường khi bắt trẻ em phải sử dụng "đúng" tay.
+Ngay cả từ "phải" cũng có nghĩa là "đúng", hay "tốt", không chỉ trong tiếng Anh, mà còn trong nhiều ngôn ngữ khác.
+Nhưng nếu thuận tay trái là sai, vậy tại sao ngay từ đầu nó lại xuất hiện?
+Ngày nay, 1/10 dân số thế giới thuận tay trái.
+Các chứng tích khảo cổ cho thấy điều này đã có sẵn từ 500.000 năm trước, với khoảng 10% hài cốt để lại cho thấy những khác biệt về độ dài cánh tay và mật độ xương, và các công cụ cổ đại và đồ thủ công thể hiện bằng chứng về việc thuận tay trái.
+Và bất kể bạn nghĩ như thế nào, bạn không thể lựa chọn tay thuận.
+Nó có thể được dự đoán trước khi sinh, dựa trên tư thế bào thai trong bụng mẹ.
+Vậy nếu tay thuận là bẩm sinh thì liệu nó có di truyền?
 À, có hoặc không.
-Song sinh đồng trứng, tuy cùng 1 bộ gene,
-có thể có tay thuận khác nhau.
-Thực tế, xác suất này cũng tương đương
-với các cặp anh chị em khác.
-Nhưng xác suất thuận tay phải hay trái
-được xác định bởi
-tay thuận của bố mẹ
-với tỉ lệ cố định đáng kinh ngạc.
-Nếu bố thuận tay trái
-nhưng mẹ lại thuận tay phải,
-bạn sẽ có xác suất 17% thuận tay trái,
-trong khi cả hai đều thuận tay phải
-thì con cái có 10% thuận tay trái.
-Việc thuận tay nào
-có lẽ là ngẫu nhiên
-nhưng xác suất
-lại quy định bởi bộ gene,
-Tất cả những điều này cho thấy
-quá trình tiến hóa đã cố tình tạo ra
-phần thiểu số người thuận tay trái,
-và duy trì tỷ lệ này qua
-hàng thiên niên kỉ.
-Và dù có nhiều thuyết
-cố gắng giải thích
-nguyên nhân tồn tại của tay thuận
-hay tại sao đa số lại
-thuận tay phải,
-một mô hình toán học gần đây
-chỉ ra tỷ lệ này
-phản ánh sự cân bằng
-giữa sức ép cạnh tranh và tương tác
-lên quá trình tiến hóa ở loài người.
-Lợi thế của việc thuận tay trái
-thể hiện rõ nhất trong các hoạt động
-mang tính đối kháng,
-như trong các trận đánh
-hay thi đấu thể thao.
-Ví dụ, khoảng 50% cầu thủ đánh bóng chày
-hàng đầu đều thuận tay trái.
-Tại sao ?
-Hãy xem nó như một
-lợi thế bất ngờ.
-Vì người thuận tay trái là thiểu số,
-nên cả người thuận tay phải
-lẫn người thuận tay trái
-đều sẽ dành hầu hết thời gian đối kháng
-và luyện tập với người thuận tay phải.
-Nên khi họ đối mặt với nhau,
-người thuận tay trái đã được chuẩn bị tốt
-để đấu với đối thủ thuận tay phải,
-trong khi người thuận tay phải
-sẽ trở nên lúng túng.
-Thuyết đối kháng này,
-nơi tỷ lệ chênh lệch
-đã tạo nên lợi thế cho
-các đối thủ thuận tay trái,
-là ví dụ cho thuyết chọn lọc
-dựa trên tần số nghịch biến.
-Nhưng theo nguyên tắc tiến hóa,
-những nhóm có lợi thế tương đối
-có khuynh hướng phát triển tới khi
-lợi thế này biến mất.
-Nếu con người chỉ chiến đấu và cạnh tranh
-trong suốt quá trình tiến hóa,
-chọn lọc tự nhiên sẽ để
-người thuận tay trái có lợi thế phát triển
-đến khi có quá nhiều người,
-và lợi thế đó không còn hiếm nữa.
-Vậy, trong một thế giới đầy cạnh tranh,
-50% dân số sẽ thuận tay trái.
-Nhưng quá trình tiến hóa của loài người
-còn có sự tương tác, cũng như cạnh tranh.
-Và sức ép tương tác
-đẩy sự phân bố tay thuận
-theo hướng ngược lại.
-Khi chơi golf, thành tích
-không phụ thuộc vào đối thủ,
-chỉ có 4% người chơi hàng đầu
-thuận tay trái,
-một ví dụ rộng hơn cho hiện tượng
-chia sẻ công cụ phổ biến.
-Cũng như những tay golf trẻ
-dễ dàng tìm thấy bộ gậy
-cho người thuận tay phải hơn,
-nhiều công cụ quan trọng
-đã định hình xã hội
-đã được thiết kế cho số đông
-người thuận tay phải.
-Bởi vì người thuận tay trái
-sử dụng bất tiện,
-và tỉ lệ mắc lỗi cao hơn,
-họ thường ít thành công hơn
-trong một thế giới tương tác,
-và cuối cùng sẽ biến mất khỏi dân số.
-Vì vậy, bằng cách dự đoán
-chính xác tỷ lệ
-người thuận tay trái trong dân số,
-đồng thời đối chiếu dữ liệu
-từ nhiều môn thể thao,
-mô hình này cho thấy
-tỷ lệ người thuận tay trái tồn tại
-tuy nhỏ nhưng ổn định
-phản ánh một trạng thái cân bằng
-là kết quả của quá trình
-cạnh tranh và tương tác
-liên tục qua thời gian.
-Và điều thú vị nhất ở đây
-là việc những con số này nói lên điều gì
-về sự đa dạng dân số.
-Từ sự phân bố bất đối xứng về chân thuận
-ở các loài thú có tính tương tác,
-tới tỉ lệ người thuận tay trái
-có xu hướng tăng nhẹ
-trong xã hội cạnh tranh
-săn bắn hái lượm,
-chúng ta có thể tìm thấy đáp án
-cho những khúc mắc về tiến hóa loài người
-nằm ngay trong bàn tay chúng ta.
-
+Song sinh đồng trứng, tuy cùng 1 bộ gene, có thể có tay thuận khác nhau.
+Thực tế, xác suất này cũng tương đương với các cặp anh chị em khác.
+Nhưng xác suất thuận tay phải hay trái được xác định bởi tay thuận của bố mẹ với tỉ lệ cố định đáng kinh ngạc.
+Nếu bố thuận tay trái nhưng mẹ lại thuận tay phải, bạn sẽ có xác suất 17% thuận tay trái, trong khi cả hai đều thuận tay phải thì con cái có 10% thuận tay trái.
+Việc thuận tay nào có lẽ là ngẫu nhiên nhưng xác suất lại quy định bởi bộ gene.
+Tất cả những điều này cho thấy quá trình tiến hóa đã cố tình tạo ra phần thiểu số người thuận tay trái, và duy trì tỷ lệ này qua hàng thiên niên kỉ.
+Và dù có nhiều thuyết cố gắng giải thích nguyên nhân tồn tại của tay thuận hay tại sao đa số lại thuận tay phải, một mô hình toán học gần đây chỉ ra tỷ lệ này phản ánh sự cân bằng giữa sức ép cạnh tranh và tương tác lên quá trình tiến hóa ở loài người.
+Lợi thế của việc thuận tay trái thể hiện rõ nhất trong các hoạt động mang tính đối kháng, như trong các trận đánh hay thi đấu thể thao.
+Ví dụ, khoảng 50% cầu thủ đánh bóng chày hàng đầu đều thuận tay trái.
+Tại sao?
+Hãy xem nó như một lợi thế bất ngờ.
+Vì người thuận tay trái là thiểu số, nên cả người thuận tay phải lẫn người thuận tay trái đều sẽ dành hầu hết thời gian đối kháng và luyện tập với người thuận tay phải.
+Nên khi họ đối mặt với nhau, người thuận tay trái đã được chuẩn bị tốt để đấu với đối thủ thuận tay phải, trong khi người thuận tay phải sẽ trở nên lúng túng.
+Thuyết đối kháng này, nơi tỷ lệ chênh lệch đã tạo nên lợi thế cho các đối thủ thuận tay trái, là ví dụ cho thuyết chọn lọc dựa trên tần số nghịch biến.
+Nhưng theo nguyên tắc tiến hóa, những nhóm có lợi thế tương đối có khuynh hướng phát triển tới khi lợi thế này biến mất.
+Nếu con người chỉ chiến đấu và cạnh tranh trong suốt quá trình tiến hóa, chọn lọc tự nhiên sẽ để người thuận tay trái có lợi thế phát triển đến khi có quá nhiều người, và lợi thế đó không còn hiếm nữa.
+Vậy, trong một thế giới đầy cạnh tranh, 50% dân số sẽ thuận tay trái.
+Nhưng quá trình tiến hóa của loài người còn có sự tương tác, cũng như cạnh tranh.
+Và sức ép tương tác đẩy sự phân bố tay thuận theo hướng ngược lại.
+Khi chơi golf, thành tích không phụ thuộc vào đối thủ, chỉ có 4% người chơi hàng đầu thuận tay trái, một ví dụ rộng hơn cho hiện tượng chia sẻ công cụ phổ biến.
+Cũng như những tay golf trẻ dễ dàng tìm thấy bộ gậy cho người thuận tay phải hơn, nhiều công cụ quan trọng đã định hình xã hội đã được thiết kế cho số đông người thuận tay phải.
+Bởi vì người thuận tay trái sử dụng bất tiện, và tỉ lệ mắc lỗi cao hơn, họ thường ít thành công hơn trong một thế giới tương tác, và cuối cùng sẽ biến mất khỏi dân số.
+Vì vậy, bằng cách dự đoán chính xác tỷ lệ người thuận tay trái trong dân số, đồng thời đối chiếu dữ liệu từ nhiều môn thể thao, mô hình này cho thấy tỷ lệ người thuận tay trái tồn tại tuy nhỏ nhưng ổn định phản ánh một trạng thái cân bằng là kết quả của quá trình cạnh tranh và tương tác liên tục qua thời gian.
+Và điều thú vị nhất ở đây là việc những con số này nói lên điều gì về sự đa dạng dân số.
+Từ sự phân bố bất đối xứng về chân thuận ở các loài thú có tính tương tác, tới tỉ lệ người thuận tay trái có xu hướng tăng nhẹ trong xã hội cạnh tranh săn bắn hái lượm, chúng ta có thể tìm thấy đáp án cho những khúc mắc về tiến hóa loài người nằm ngay trong bàn tay chúng ta.
   `,
 };
