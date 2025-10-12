9522f5c0f8a4115b60d8685ef7c84e4678cf6a49
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-12 16:08:30 +0700

    Update book3.ts

--- src/ebook/ebooks/book3.ts
+++ src/ebook/ebooks/book3.ts
@@ -9,290 +9,94 @@ export const book3Data: Book = {
     'Raw': 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/main/src/audio-ted/audio/origin-voice/003.mp3',
   
   },
-  content: `
-
-
-I heard this woman singing classical music
-and everything for me changed. 
+  content: `I heard this woman singing classical music and everything for me changed.
 It was that profound.
-It was such an incredibly
-impactful happening,
-that that day I knew that my life changed.
-That day, it gave my life
-a whole direction
-and I thought whatever this is,
-this is what I want to do.
+It was such an incredibly impactful happening, that that day I knew that my life changed.
+That day, it gave my life a whole direction and I thought whatever this is, this is what I want to do.
 Hey you! Yes, you.
 What's that light inside of you?
-Is it a dream? A beat? A beautiful sound?
-A heartbreaking song?
-Whatever it is, 
-we're here with working musicians
-to help you grow and share that gift
-with this wild, wonderful world.
-My name is Denyce Graves-Montgomery,
-and I have spent more than 45 years
-on the stage as an opera singer.
-Parallel to that, I have all always been
-in education and have also taught voice.
-Even if you're a natural singer and
-you know how to sing very, very well,
-some kind of training is going
-to be necessary.
-It will involve having some 
-external ears,
-who can listen to your voice and say:
-your instrument is suited in this range.
-So in terms of finding your
-specific voice type,
-you would go to a teacher or to a coach,
-and they would give you
-different vocal exercises
-up and down the scale
-to see what's comfortable,
-and where the voice also shines and
-where the voice is at its most beautiful.
+Is it a dream? A beat? A beautiful sound? A heartbreaking song?
+Whatever it is, we're here with working musicians to help you grow and share that gift with this wild, wonderful world.
+My name is Denyce Graves-Montgomery, and I have spent more than 45 years on the stage as an opera singer.
+Parallel to that, I have all always been in education and have also taught voice.
+Even if you're a natural singer and you know how to sing very, very well, some kind of training is going to be necessary.
+It will involve having some external ears, who can listen to your voice and say: your instrument is suited in this range.
+So in terms of finding your specific voice type, you would go to a teacher or to a coach, and they would give you different vocal exercises up and down the scale to see what's comfortable, and where the voice also shines and where the voice is at its most beautiful.
 I'm considered a mezzo soprano.
-We have the coloratura soprano,
-which is the highest soprano.
-Then you have soprano, and then
-you've got the mezzo sopranos,
-the altos, the contraltos.  
-Then you have the tenors,
-the countertenors,
-the light tenors, dramatic tenors,
-the heldentenors,  
-all the different classifications,
-baritones, and then the bases, right?
-There's a lot to learn about your voice
-and what your voice is suited for.
-There are some young people
-who come to me, for example,
-who might start out as mezzos,
-who might start out as altos,
-and then we discover that they, in fact,
-have this incredible range.
+We have the coloratura soprano, which is the highest soprano.
+Then you have soprano, and then you've got the mezzo sopranos, the altos, the contraltos.
+Then you have the tenors, the countertenors, the light tenors, dramatic tenors, the heldentenors, all the different classifications, baritones, and then the bases, right?
+There's a lot to learn about your voice and what your voice is suited for.
+There are some young people who come to me, for example, who might start out as mezzos, who might start out as altos, and then we discover that they, in fact, have this incredible range.
 And that they, you know, are sopranos.
-It takes time for the voice itself
-to ripen and to grow,
-and to be able to get to that point
-where you're able to cut
-across a really, really big orchestra.
-There has to be a certain amount of heft,
-there has to be a certain amount of steel
-in the sound to be able
-to reach to the person
-sitting in the top balcony
-in the last row.
-I am a person who has a 
-big, old honking voice, right?
-My forehead and my cheekbones
-and my nose
-and all of these incredible
-resonating cavities
-which creates great resonance
-that's able to cut.
-And when you first start studying,
-there's the technical part,
-but that continues throughout
-the whole of your life.
-The vocal technique of singing
-is what we call chiaroscuro.
-Chiaro, clear,
-and scuro, which is darker,
-to create a balanced sound.
-And to be able to do that,
-you have to be able to come in contact
-with what your body is able to do,
-and that is being able to use your body
-as the microphone.
-And the most important thing is technique,
-technique, technique, technique.
-And that would be fundamentally the
-breath and understanding what that is.
-And then understanding how
-to support that in your body,
-and with your diaphragm
-and in your rib cage and in the back
-and in your lower abdomen support. 
-Being able to create that
-wonderful hollow sound,
-where you're able to use
-those resonators,
-and then the articulators,
-the tongue and the teeth,
-and all those different things.
-If, for example, if you blow up a balloon,
-you blow the balloon up.
+It takes time for the voice itself to ripen and to grow, and to be able to get to that point where you're able to cut across a really, really big orchestra.
+There has to be a certain amount of heft, there has to be a certain amount of steel in the sound to be able to reach to the person sitting in the top balcony in the last row.
+I am a person who has a big, old honking voice, right?
+My forehead and my cheekbones and my nose and all of these incredible resonating cavities which creates great resonance that's able to cut.
+And when you first start studying, there's the technical part, but that continues throughout the whole of your life.
+The vocal technique of singing is what we call chiaroscuro.
+Chiaro, clear, and scuro, which is darker, to create a balanced sound.
+And to be able to do that, you have to be able to come in contact with what your body is able to do, and that is being able to use your body as the microphone.
+And the most important thing is technique, technique, technique, technique.
+And that would be fundamentally the breath and understanding what that is.
+And then understanding how to support that in your body, and with your diaphragm and in your rib cage and in the back and in your lower abdomen support.
+Being able to create that wonderful hollow sound, where you're able to use those resonators, and then the articulators, the tongue and the teeth, and all those different things.
+If, for example, if you blow up a balloon, you blow the balloon up.
 And you fill the balloon with air.
-If you don't hold on to the mouth
-of the balloon,
-all the air will come out and the balloon
-will fly all over the place.
-And with singing, what you want to do
-is make sure that you hold on
-to the mouth of the balloon.
-So you create air and you've got
-to use that air,
-and then you've got to spend that air,
-but you got to make sure
-that you don't use it all up.
-And so that's the hand on the mouth
-of the balloon,
-so that you are able to dole
-that air out evenly.
-So you don't let it all go out
-on the first phrase,
-but you're able to gauge that breath
-and spend that breath 
-throughout the different phrases.
-The bottom line, the bottom,
-bottom, bottom line:
-if it isn't real, we know.
-It has to be real, it has to be honest.  
-That is the thing that is transformative
-for people when they go to the theater.
-People come and they're riveted
-because they're seeing something
-in real time happen that is authentic.
-And that's the thing that changes
-people's lives.
-The truth. The truth.
- 
-
-  `,
-  contentVi: `
-
-Tôi nghe người phụ nữ này hát nhạc cổ điển
-và mọi thứ đối với tôi đã thay đổi.
+If you don't hold on to the mouth of the balloon, all the air will come out and the balloon will fly all over the place.
+And with singing, what you want to do is make sure that you hold on to the mouth of the balloon.
+So you create air and you've got to use that air, and then you've got to spend that air, but you got to make sure that you don't use it all up.
+And so that's the hand on the mouth of the balloon, so that you are able to dole that air out evenly.
+So you don't let it all go out on the first phrase, but you're able to gauge that breath and spend that breath throughout the different phrases.
+The bottom line, the bottom, bottom, bottom line: if it isn't real, we know.
+It has to be real, it has to be honest.
+That is the thing that is transformative for people when they go to the theater.
+People come and they're riveted because they're seeing something in real time happen that is authentic.
+And that's the thing that changes people's lives.
+The truth. The truth.`,
+  contentVi: `Tôi nghe người phụ nữ này hát nhạc cổ điển và mọi thứ đối với tôi đã thay đổi.
 Nó thật sâu sắc.
-Đó là một sự kiện cực kỳ có
-tác động đến mức
-khiến tôi biết đời mình đã khác.
-Ngày đó, nó cho đời tôi một lối đi mới.
-Và tôi nghĩ “Dù là gì đi nữa,
-đây chính là điều tôi muốn theo đuổi.”
+Đó là một sự kiện cực kỳ có tác động đến mức, khiến tôi biết đời mình đã khác.
+Ngày đó, nó cho đời tôi một lối đi mới và tôi nghĩ dù là gì đi nữa, đây chính là điều tôi muốn theo đuổi.
 Này bạn! Đúng rồi, bạn á.
 Ánh sáng nào đang cháy trong tim bạn?
-Đó có phải là một giấc mơ?
-Một nhịp đập? Một âm thanh đẹp?
-Một bài hát đau lòng?
-Dù đó là gì, chúng tôi ở đây
-cùng những nhạc sĩ
-để giúp bạn phát triển và chia sẻ món quà
-với thế giới muôn màu kỳ diệu này.
-Tên tôi là Denyce Graves-Montgomery,
-và tôi đã dành hơn 45 năm trên sân khấu
-với tư cách là một ca sĩ opera.
-Song song với đó, tôi luôn gắn bó
-với giáo dục và dạy thanh nhạc.
-Dù bạn bẩm sinh đã có giọng hát,
-hay hát thật sự rất giỏi đi nữa,
-thì việc rèn luyện vẫn luôn cần thiết.
-Nó sẽ liên quan đến việc
-có một đôi tai bên ngoài,
-lắng nghe và góp ý cho bạn biết:
-Giọng hát của bạn phù hợp nhất
-trong quãng âm này.
-Để xác định loại giọng của riêng bạn,
-hãy đến gặp giáo viên
-hoặc huấn luyện viên.
-Họ sẽ đưa cho bạn nhiều bài luyện thanh,
-lên xuống các nốt để tìm sự thoải mái.
-Qua đó, bạn sẽ thấy giọng mình tỏa sáng
-và đẹp nhất ở những quãng nào.
+Đó có phải là một giấc mơ? Một nhịp đập? Một âm thanh đẹp? Một bài hát đau lòng?
+Dù đó là gì, chúng tôi ở đây cùng những nhạc sĩ để giúp bạn phát triển và chia sẻ món quà với thế giới muôn màu kỳ diệu này.
+Tên tôi là Denyce Graves-Montgomery, và tôi đã dành hơn 45 năm trên sân khấu với tư cách là một ca sĩ opera.
+Song song với đó, tôi luôn gắn bó với giáo dục và dạy thanh nhạc.
+Dù bạn bẩm sinh đã có giọng hát hay hát thật sự rất giỏi đi nữa, một số loại hình đào tạo sẽ là cần thiết.
+Nó sẽ liên quan đến việc có một đôi tai bên ngoài, lắng nghe và góp ý cho bạn biết: Giọng hát của bạn phù hợp nhất trong quãng âm này.
+Để xác định loại giọng của riêng bạn, hãy đến gặp giáo viên hoặc huấn luyện viên, họ sẽ đưa cho bạn nhiều bài luyện thanh lên xuống các nốt để tìm sự thoải mái, và nơi giọng hát cũng tỏa sáng và nơi giọng hát đẹp nhất.
 Tôi được xếp vào giọng nữ trung (mezzo).
-Có giọng nữ cao kỹ thuật,
-tức coloratura soprano, cao nhất.
-Rồi đến soprano, sau đó là mezzo-soprano,
-tiếp theo là alto và contralto.
-Rồi đến tenor, countertenor,
-light tenor, dramatic tenors, heldentenor.
-Và còn nhiều loại khác:
-baritone, rồi đến bass, đúng không?
-Có rất nhiều điều để học về giọng hát
-và quãng âm mà nó phù hợp.
-Ví dụ, có vài bạn trẻ đến tìm tôi,
-ban đầu được xếp là mezzo hoặc alto.
-Nhưng rồi chúng tôi phát hiện ra
-họ thật sự có quãng giọng tuyệt vời.
+Có giọng nữ cao kỹ thuật, tức coloratura soprano, cao nhất.
+Rồi đến soprano, sau đó là mezzo-soprano, tiếp theo là alto và contralto.
+Rồi đến tenor, countertenor, light tenor, dramatic tenors, heldentenor, và còn nhiều loại khác: baritone, rồi đến bass, đúng không?
+Có rất nhiều điều để học về giọng hát và quãng âm mà nó phù hợp.
+Ví dụ, có vài bạn trẻ đến tìm tôi, ban đầu được xếp là mezzo hoặc alto, nhưng rồi chúng tôi phát hiện ra họ thật sự có quãng giọng tuyệt vời.
 Và họ, bạn biết đấy, là soprano.
-Giọng hát cần thời gian để
-chín dần và phát triển,
-Chỉ khi đó, bạn mới có thể
-cất giọng vang lên
-giữa cả dàn nhạc lớn.
-Âm thanh phải có sức nặng,
-phải có độ rắn chắc,
-thì mới chạm tới người
-ngồi tận hàng ghế trên cùng, xa nhất.
-Tôi là người có một giọng hát
-to, dày và vang dội.
-Trán, gò má và mũi của tôi
-và tất cả những khoang
-cộng hưởng đáng kinh ngạc,
-tạo âm thanh có độ vang lớn,
-đủ sức xuyên qua không gian.
-Khi mới học, bạn tập kỹ thuật,
-nhưng việc đó bạn vẫn
-tiếp tục học kéo dài cả đời.
-Kỹ thuật thanh nhạc của ca hát
-là cái mà chúng ta gọi là chiaroscuro.
-Chiaro là sáng, scuro là tối,
-kết hợp để tạo ra âm thanh cân bằng.
-Và để có thể làm được điều đó,
-bạn cần hiểu rõ khả năng
-của cơ thể có thể làm,
-và bạn sẽ biết sử dụng
-cơ thể của bạn như một chiếc micrô.
-Và điều quan trọng nhất là kỹ thuật,
-kỹ thuật, kỹ thuật, kỹ thuật.
-Cốt lõi là hơi thở và sự hiểu biết
-về cách kiểm soát nó.
-Và hiểu cách nào để hỗ
-trợ điều đó trong cơ thể bạn
-với cơ hoành, trong lồng
-xương sườn, lưng
-và cả phần cơ bụng dưới
-để giữ giọng bền vững.
-Có thể tạo ra âm vang rỗng tuyệt đẹp đó
-nhờ tận dụng các khoang cộng hưởng.
-Rồi đến các bộ phận phát âm: lưỡi, răng
-và nhiều yếu tố khác.
-Ví dụ nhé, giống như khi bạn thổi
-một quả bóng bay.
-Lấp đầy nó bằng không khí.
-Nếu không giữ chặt miệng bóng,
-hơi sẽ thoát ra, bóng bay tứ tung.
-Với việc hát, những gì bạn muốn làm
-là đảm bảo rằng bạn giữ
-chặt miệng của quả bóng bay.
-Bạn tạo ra hơi thở và phải biết dùng nó,
-phân bổ nó hợp lý.
-Quan trọng là đừng để xài hết hơi
-chỉ trong một lần.
-Bàn tay giữ miệng bóng tượng trưng
-cho việc kiểm soát hơi thở đều đặn.
-Đừng để hơi thoát hết ngay
-trong câu hát đầu tiên,
-hãy biết đo lường và phân bổ hơi thở
-cho suốt nhiều câu hát khác nhau.
-Điều cốt lõi, cốt lõi, cốt lõi là:
-nếu không thật, khán giả sẽ nhận ra.
-Nó phải chân thật,
-phải xuất phát từ trái tim.
-Đó chính là điều chạm đến con người,
-khi họ bước vào nhà hát.
-Khán giả đến và bị cuốn hút
-vì họ chứng kiến
-điều thật diễn ra ngay lúc đó.
-Chính sự chân thực ấy
-mới làm thay đổi đời người.
-Sự thật. Sự thật.
-
-
-
-
- 
-  `,
+Giọng hát cần thời gian để chín dần và phát triển, và để có thể đạt đến điểm mà bạn có thể vượt qua một dàn nhạc thực sự, thực sự lớn.
+Âm thanh phải có sức nặng, phải có độ rắn chắc, thì mới chạm tới người ngồi tận hàng ghế trên cùng, xa nhất.
+Tôi là người có một giọng hát to, dày và vang dội, đúng không?
+Trán, gò má và mũi của tôi và tất cả những khoang cộng hưởng đáng kinh ngạc, tạo âm thanh có độ vang lớn, đủ sức xuyên qua không gian.
+Khi mới học, bạn tập kỹ thuật, nhưng việc đó bạn vẫn tiếp tục học kéo dài cả đời.
+Kỹ thuật thanh nhạc của ca hát là cái mà chúng ta gọi là chiaroscuro.
+Chiaro là sáng, và scuro là tối, để tạo ra âm thanh cân bằng.
+Và để có thể làm được điều đó, bạn cần hiểu rõ khả năng của cơ thể có thể làm, và bạn sẽ biết sử dụng cơ thể của bạn như một chiếc micrô.
+Và điều quan trọng nhất là kỹ thuật, kỹ thuật, kỹ thuật, kỹ thuật.
+Cốt lõi là hơi thở và sự hiểu biết về cách kiểm soát nó.
+Và hiểu cách nào để hỗ trợ điều đó trong cơ thể bạn, với cơ hoành, trong lồng xương sườn, lưng và cả phần cơ bụng dưới để giữ giọng bền vững.
+Có thể tạo ra âm vang rỗng tuyệt đẹp đó nhờ tận dụng các khoang cộng hưởng, rồi đến các bộ phận phát âm: lưỡi, răng và nhiều yếu tố khác.
+Ví dụ nhé, giống như khi bạn thổi một quả bóng bay, bạn thổi quả bóng bay lên.
+Và bạn lấp đầy nó bằng không khí.
+Nếu không giữ chặt miệng bóng, hơi sẽ thoát ra, bóng bay tứ tung.
+Với việc hát, những gì bạn muốn làm là đảm bảo rằng bạn giữ chặt miệng của quả bóng bay.
+Bạn tạo ra hơi thở và phải biết dùng nó, và rồi bạn phải phân bổ nó hợp lý, nhưng bạn phải đảm bảo rằng bạn không dùng hết nó.
+Và đó là bàn tay giữ miệng bóng, để bạn có thể phân phối không khí đó ra đều đặn.
+Vì vậy, bạn không để nó thoát ra hết ngay trong câu hát đầu tiên, mà bạn có thể đo lường hơi thở đó và phân bổ hơi thở đó trong suốt các câu hát khác nhau.
+Điều cốt lõi, cốt lõi, cốt lõi là: nếu không thật, khán giả sẽ nhận ra.
+Nó phải chân thật, phải xuất phát từ trái tim.
+Đó chính là điều chạm đến con người, khi họ bước vào nhà hát.
+Khán giả đến và bị cuốn hút vì họ chứng kiến điều thật diễn ra ngay lúc đó.
+Chính sự chân thực ấy mới làm thay đổi đời người.
+Sự thật. Sự thật.`,
 };
