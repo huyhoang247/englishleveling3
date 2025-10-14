f3e0432e2e3628b7b09f124ca5c60d9f9ab86e4f
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-14 15:49:19 +0700

    Create book6.ts

---
+++ src/ebook/ebooks/book6.ts
@@ -0,0 +1,177 @@
+import { Book } from '../types';
+
+export const book6Data: Book = {
+  id: 'book6',
+  title: "Love Me Like You Do",
+  author: 'Ellie Goulding',
+  category: 'Music',
+  coverImageUrl: 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/refs/heads/main/src/audio-ted/image/005.jpg',
+  audioUrls: {
+    'Raw': 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/main/src/audio-ted/audio/origin-voice/005.mp3',
+  },
+  content: `
+  
+♪♪♪
+♪ YOU'RE THE LIGHT,
+YOU'RE THE NIGHT ♪
+♪ YOU'RE THE COLOR OF MY BLOOD ♪
+♪ YOU'RE THE CURE,
+YOU'RE THE PAIN ♪
+♪ YOU'RE THE ONLY THING
+I WANNA TOUCH ♪
+♪ NEVER KNEW THAT IT COULD
+MEAN SO MUCH, SO MUCH ♪
+♪ YOU'RE THE FEAR,
+I DON'T CARE ♪
+♪ 'CAUSE I'VE NEVER
+BEEN SO HIGH ♪
+♪ FOLLOW ME THROUGH THE DARK ♪
+♪ LET ME TAKE YOU PAST
+OUR SATELLITES ♪
+♪ YOU CAN SEE THE WORLD
+YOU BROUGHT TO LIFE, TO LIFE ♪
+♪ SO LOVE ME LIKE YOU DO,
+LO-LO-LOVE ME LIKE YOU DO ♪
+♪ LOVE ME LIKE YOU DO,
+LO- LO- LOVE ME LIKE YOU DO ♪
+♪ TOUCH ME LIKE YOU DO,
+TO-TO-TOUCH ME LIKE YOU DO ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪ FADING IN, FADING OUT ♪
+♪ ON THE EDGE OF PARADISE ♪
+♪ EVERY INCH OF YOUR SKIN IS
+A HOLY GRAIL I'VE GOT TO FIND ♪
+♪ ONLY YOU CAN SET MY HEART
+ON FIRE, ON FIRE ♪
+♪ YEAH, I'LL LET
+YOU SET THE PACE ♪
+♪ 'CAUSE I'M NOT
+THINKING STRAIGHT ♪
+♪ MY HEAD'S SPINNING AROUND
+I CAN'T SEE CLEAR NO MORE ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪ LOVE ME LIKE YOU DO, LO-
+LO- LOVE ME LIKE YOU DO
+(LIKE YOU DO) ♪
+♪ LOVE ME LIKE YOU DO,
+LO- LO- LOVE ME LIKE YOU DO ♪
+♪ TOUCH ME LIKE YOU DO,
+TO-TO-TOUCH ME LIKE YOU DO ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪ LOVE ME LIKE YOU DO, LO-
+LO- LOVE ME LIKE YOU DO
+(LIKE YOU DO) ♪
+♪ LOVE ME LIKE YOU DO, LO-
+LO- LOVE ME LIKE YOU DO (YEAH) ♪
+♪ TOUCH ME LIKE YOU DO,
+TO-TO-TOUCH ME LIKE YOU DO ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪♪♪
+♪ AH AHH ♪
+♪ I'LL LET YOU SET THE PACE ♪
+♪ 'CAUSE I'M NOT
+THINKING STRAIGHT ♪
+♪ MY HEAD'S SPINNING AROUND
+I CAN'T SEE CLEAR NO MORE ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪ LOVE ME LIKE YOU DO, LO-
+LO- LOVE ME LIKE YOU DO
+(LIKE YOU DO) ♪
+♪ LOVE ME LIKE YOU DO, LO-
+LO- LOVE ME LIKE YOU DO (YEAH) ♪
+♪ TOUCH ME LIKE YOU DO,
+TO-TO-TOUCH ME LIKE YOU DO ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪ LOVE ME LIKE YOU DO,
+LO- LO- LOVE ME LIKE YOU DO
+(LIKE YOU DO) ♪
+♪ LOVE ME LIKE YOU DO, LO-
+LO- LOVE ME LIKE YOU DO (OH) ♪
+♪ TOUCH ME LIKE YOU DO,
+TO-TO-TOUCH ME LIKE YOU DO ♪
+♪ WHAT ARE YOU WAITING FOR? ♪
+♪♪♪
+  
+  `,
+  contentVi: `
+
+♪♪♪
+♪ Anh là ánh sáng,
+anh là màn đêm ♪
+♪ Anh là sắc màu trong huyết quản em ♪
+♪ Anh là liều thuốc,
+cũng là nỗi đau ♪
+♪ Anh là điều duy nhất
+em khát khao chạm tới ♪
+♪ Chưa từng biết điều đó lại có thể
+ý nghĩa biết bao, biết bao ♪
+♪ Anh là nỗi sợ,
+nhưng em chẳng màng ♪
+♪ Bởi em chưa bao giờ
+thăng hoa đến vậy ♪
+♪ Hãy theo em qua màn đêm u tối ♪
+♪ Để em đưa anh vượt qua
+những vì tinh tú của đôi ta ♪
+♪ Anh sẽ thấy thế giới anh đã
+thắp lên sự sống, thắp lên sự sống ♪
+♪ Vậy hãy yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu ♪
+♪ Yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu ♪
+♪ Hãy chạm vào em như cách anh vẫn thế,
+chạ-chạ-chạm vào em như cách anh vẫn thế ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪ Khi mờ khi tỏ ♪
+♪ Nơi rìa chốn thiên đường ♪
+♪ Từng tấc da thịt anh là
+Chén Thánh mà em phải kiếm tìm ♪
+♪ Chỉ riêng anh mới có thể khiến tim em
+bùng cháy, bùng cháy ♪
+♪ Vâng, em sẽ để
+anh dẫn lối ♪
+♪ Bởi em chẳng thể
+nghĩ suy thông suốt ♪
+♪ Đầu óc em quay cuồng,
+chẳng thể nhìn rõ điều chi nữa rồi ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪ Yêu em như cách anh vẫn yêu, yê-
+yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu) ♪
+♪ Yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu ♪
+♪ Hãy chạm vào em như cách anh vẫn thế,
+chạ-chạ-chạm vào em như cách anh vẫn thế ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪ Yêu em như cách anh vẫn yêu, yê-
+yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu) ♪
+♪ Yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu (Vâng) ♪
+♪ Hãy chạm vào em như cách anh vẫn thế,
+chạ-chạ-chạm vào em như cách anh vẫn thế ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪♪♪
+♪ Ah AHH ♪
+♪ Em sẽ để anh dẫn lối ♪
+♪ Bởi em chẳng thể
+nghĩ suy thông suốt ♪
+♪ Đầu óc em quay cuồng,
+chẳng thể nhìn rõ điều chi nữa rồi ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪ Yêu em như cách anh vẫn yêu, yê-
+yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu) ♪
+♪ Yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu (Vâng) ♪
+♪ Hãy chạm vào em như cách anh vẫn thế,
+chạ-chạ-chạm vào em như cách anh vẫn thế ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪ Yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu) ♪
+♪ Yêu em như cách anh vẫn yêu,
+yê-yê-yêu em như cách anh vẫn yêu (Ồ) ♪
+♪ Hãy chạm vào em như cách anh vẫn thế,
+chạ-chạ-chạm vào em như cách anh vẫn thế ♪
+♪ Anh còn chần chờ điều chi? ♪
+♪♪♪
+
+ 
+  `,
+};
