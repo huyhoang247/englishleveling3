df7aee3b24140a236e61e5810e8bda0463c16b17
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-14 17:22:35 +0700

    Update book6.ts

--- src/ebook/ebooks/book6.ts
+++ src/ebook/ebooks/book6.ts
@@ -11,95 +11,95 @@ export const book6Data: Book = {
   },
   content: `
   
-You're the light, you're the night
-You're the color of my blood
-You're the cure, you're the pain
-You're the only thing i wanna touch
-Never knew that it could mean so much, so much
-You're the fear, i don't care
-'cause i've never been so high
-Follow me through the dark
-Let me take you past our satellites
-You can see the world you brought to life, to life
-So love me like you do, lo-lo-love me like you do
-Love me like you do, lo- lo- love me like you do
-Touch me like you do, to-to-touch me like you do
+You're the light, you're the night.
+You're the color of my blood.
+You're the cure, you're the pain.
+You're the only thing I wanna touch.
+Never knew that it could mean so much, so much.
+You're the fear, I don't care.
+'Cause I've never been so high.
+Follow me through the dark.
+Let me take you past our satellites.
+You can see the world you brought to life, to life.
+So love me like you do, lo-lo-love me like you do.
+Love me like you do, lo- lo- love me like you do.
+Touch me like you do, to-to-touch me like you do.
 What are you waiting for?
-Fading in, fading out
-On the edge of paradise
-Every inch of your skin is a holy grail i've got to find
-Only you can set my heart on fire, on fire
-Yeah, i'll let you set the pace
-'cause i'm not thinking straight
-My head's spinning around i can't see clear no more
+Fading in, fading out.
+On the edge of paradise.
+Every inch of your skin is a holy grail I've got to find.
+Only you can set my heart on fire, on fire.
+Yeah, I'll let you set the pace.
+'Cause I'm not thinking straight.
+My head's spinning around, I can't see clear no more.
 What are you waiting for?
-Love me like you do, lo- lo- love me like you do (like you do)
-Love me like you do, lo- lo- love me like you do
-Touch me like you do, to-to-touch me like you do
+Love me like you do, lo- lo- love me like you do (like you do).
+Love me like you do, lo- lo- love me like you do.
+Touch me like you do, to-to-touch me like you do.
 What are you waiting for?
-Love me like you do, lo- lo- love me like you do (like you do)
-Love me like you do, lo- lo- love me like you do (yeah)
-Touch me like you do, to-to-touch me like you do
+Love me like you do, lo- lo- love me like you do (like you do).
+Love me like you do, lo- lo- love me like you do (yeah).
+Touch me like you do, to-to-touch me like you do.
 What are you waiting for?
-Ah ahh
-I'll let you set the pace
-'cause i'm not thinking straight
-My head's spinning around i can't see clear no more
+Ah ahh.
+I'll let you set the pace.
+'Cause I'm not thinking straight.
+My head's spinning around, I can't see clear no more.
 What are you waiting for?
-Love me like you do, lo- lo- love me like you do (like you do)
-Love me like you do, lo- lo- love me like you do (yeah)
-Touch me like you do, to-to-touch me like you do
+Love me like you do, lo- lo- love me like you do (like you do).
+Love me like you do, lo- lo- love me like you do (yeah).
+Touch me like you do, to-to-touch me like you do.
 What are you waiting for?
-Love me like you do, lo- lo- love me like you do (like you do)
-Love me like you do, lo- lo- love me like you do (oh)
-Touch me like you do, to-to-touch me like you do
+Love me like you do, lo- lo- love me like you do (like you do).
+Love me like you do, lo- lo- love me like you do (oh).
+Touch me like you do, to-to-touch me like you do.
 What are you waiting for?
   
   `,
   contentVi: `
 
-Anh là ánh sáng, anh là màn đêm
-Anh là sắc màu trong huyết quản em
-Anh là liều thuốc, cũng là nỗi đau
-Anh là điều duy nhất em khát khao chạm tới
-Chưa từng biết điều đó lại có thể ý nghĩa biết bao, biết bao
-Anh là nỗi sợ, nhưng em chẳng màng
-Bởi em chưa bao giờ thăng hoa đến vậy
-Hãy theo em qua màn đêm u tối
-Để em đưa anh vượt qua những vì tinh tú của đôi ta
-Anh sẽ thấy thế giới anh đã thắp lên sự sống, thắp lên sự sống
-Vậy hãy yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu
-Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu
-Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế
+Anh là ánh sáng, anh là màn đêm.
+Anh là sắc màu trong huyết quản em.
+Anh là liều thuốc, cũng là nỗi đau.
+Anh là điều duy nhất em khát khao chạm tới.
+Chưa từng biết điều đó lại có thể ý nghĩa biết bao, biết bao.
+Anh là nỗi sợ, nhưng em chẳng màng.
+Bởi em chưa bao giờ thăng hoa đến vậy.
+Hãy theo em qua màn đêm u tối.
+Để em đưa anh vượt qua những vì tinh tú của đôi ta.
+Anh sẽ thấy thế giới anh đã thắp lên sự sống, thắp lên sự sống.
+Vậy hãy yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu.
+Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu.
+Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế.
 Anh còn chần chờ điều chi?
-Khi mờ khi tỏ
-Nơi rìa chốn thiên đường
-Từng tấc da thịt anh là Chén Thánh mà em phải kiếm tìm
-Chỉ riêng anh mới có thể khiến tim em bùng cháy, bùng cháy
-Vâng, em sẽ để anh dẫn lối
-Bởi em chẳng thể nghĩ suy thông suốt
-Đầu óc em quay cuồng, chẳng thể nhìn rõ điều chi nữa rồi
+Khi mờ khi tỏ.
+Nơi rìa chốn thiên đường.
+Từng tấc da thịt anh là Chén Thánh mà em phải kiếm tìm.
+Chỉ riêng anh mới có thể khiến tim em bùng cháy, bùng cháy.
+Vâng, em sẽ để anh dẫn lối.
+Bởi em chẳng thể nghĩ suy thông suốt.
+Đầu óc em quay cuồng, chẳng thể nhìn rõ điều chi nữa rồi.
 Anh còn chần chờ điều chi?
-Yêu em như cách anh vẫn yêu, yê- yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu)
-Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu
-Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế
+Yêu em như cách anh vẫn yêu, yê- yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu).
+Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu.
+Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế.
 Anh còn chần chờ điều chi?
-Yêu em như cách anh vẫn yêu, yê- yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu)
-Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (Vâng)
-Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế
+Yêu em như cách anh vẫn yêu, yê- yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu).
+Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (Vâng).
+Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế.
 Anh còn chần chờ điều chi?
-Ah AHH
-Em sẽ để anh dẫn lối
-Bởi em chẳng thể nghĩ suy thông suốt
-Đầu óc em quay cuồng, chẳng thể nhìn rõ điều chi nữa rồi
+Ah AHH.
+Em sẽ để anh dẫn lối.
+Bởi em chẳng thể nghĩ suy thông suốt.
+Đầu óc em quay cuồng, chẳng thể nhìn rõ điều chi nữa rồi.
 Anh còn chần chờ điều chi?
-Yêu em như cách anh vẫn yêu, yê- yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu)
-Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (Vâng)
-Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế
+Yêu em như cách anh vẫn yêu, yê- yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu).
+Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (Vâng).
+Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế.
 Anh còn chần chờ điều chi?
-Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu)
-Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (Ồ)
-Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế
+Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (như cách anh vẫn yêu).
+Yêu em như cách anh vẫn yêu, yê-yê-yêu em như cách anh vẫn yêu (Ồ).
+Hãy chạm vào em như cách anh vẫn thế, chạ-chạ-chạm vào em như cách anh vẫn thế.
 Anh còn chần chờ điều chi?
  
   `,
