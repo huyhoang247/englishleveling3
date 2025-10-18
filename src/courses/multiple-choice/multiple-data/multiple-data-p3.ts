3354b09e0945bd88753e39f9a960ba9eea546695
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-18 23:00:03 +0700

    Create multiple-data-p3.ts

---
+++ src/courses/multiple-choice/multiple-data/multiple-data-p3.ts
@@ -0,0 +1,20 @@
+const QuizDataPart3 = [
+  {
+    "question": "Từ \"French\" trong tiếng Anh có nghĩa là gì?",
+    "options": ["Bàn", "Tiếng Pháp", "Con đường", "Màu đỏ"],
+    "correctAnswer": "Tiếng Pháp"
+  },
+{
+    "question": "Từ \"founded\" trong tiếng Anh có nghĩa là gì?",
+    "options": ["Được thành lập", "Bị phá hủy", "Đã di chuyển", "Được sơn"],
+    "correctAnswer": "Được thành lập"
+  }
+
+
+  
+
+  
+
+];
+
+export default QuizDataPart3;
