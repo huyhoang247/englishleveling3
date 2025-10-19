1b016f1153c04cc447811bb7c7109f5f22f1d69d
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-19 09:33:16 +0700

    Create multiple-data-p4.ts

---
+++ src/courses/multiple-choice/multiple-data/multiple-data-p4.ts
@@ -0,0 +1,29 @@
+const QuizDataPart4 = [
+  
+  {
+    "question": "Từ Comprehensive trong tiếng Anh có nghĩa là gì?",
+    "options": ["Toàn diện", "Con ngựa", "Máy in", "Tiếng Ý"],
+    "correctAnswer": "Toàn diện"
+  },
+{
+    "question": "Từ \"worthy\" trong tiếng Anh có nghĩa là gì?",
+    "options": ["Xứng đáng", "Vô dụng", "Rẻ tiền", "Lạnh lùng"],
+    "correctAnswer": "Xứng đáng"
+  }
+
+
+ 
+
+
+
+
+
+
+
+  
+
+  
+
+];
+
+export default QuizDataPart4;
