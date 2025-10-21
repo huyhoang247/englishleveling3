9098c2f499f8e6c7c873af39f1afe4e4e1cf03c3
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-10-21 18:16:07 +0700

    Create book7.ts

---
+++ src/ebook/ebooks/book7.ts
@@ -0,0 +1,310 @@
+import { Book } from '../types';
+
+export const book7Data: Book = {
+  id: 'book7',
+  title: "Is it really that bad to eat cookie dough?",
+  author: 'Emma Bryce',
+  category: 'Ted-Ed',
+  coverImageUrl: 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/refs/heads/main/src/audio-ted/image/006.jpg',
+  audioUrls: {
+    'Raw': 'https://raw.githubusercontent.com/huyhoang247/englishleveling3/main/src/audio-ted/audio/origin-voice/006.mp3',
+  },
+  content: `
+  
+Somewhere on a farm in Iowa in 2010,
+a hen lays an egg.
+In just a few short weeks,
+this egg will be part
+of a massive infection event:
+thousands of people will fall ill,
+millions of eggs will be recalled,
+and several egg industry titans
+will ultimately land in jail,
+all thanks to a microscopic
+but mighty bacterium.
+Salmonella infects millions
+worldwide each year,
+causing fever, stomach cramps,
+and diarrhea.
+And these effects can be extreme:
+Salmonella is the leading cause
+of hospitalizations and deaths
+from food poisoning.
+So, let’s follow this microbe to find
+out how it makes so many people sick.
+We begin in the chicken's digestive tract,
+a major source of all
+Salmonella infections.
+In chickens, Salmonella bacteria
+often go undetected,
+allowing them to spread to eggs
+either through the developing yolk
+or by passing through feces,
+which can then contaminate shells.
+Under unhygienic farming conditions,
+this Salmonella-laden feces may also
+infect or contaminate
+other animals and crops,
+causing various food-linked outbreaks.
+Meanwhile, chicken meat can be exposed to
+intestinal Salmonella during processing.
+On its journey from farm to plate,
+the microbe can survive extreme cold,
+wet, and dry conditions.
+However, once it moves into a human body,
+Salmonella reveals its true talents
+for survival.
+The first hurdle is the stomach.
+Here, most bacterial invaders
+are killed off by stomach acid.
+But Salmonella cells
+can detect acidic conditions,
+which triggers the production
+of acid shock proteins.
+These molecules shield the bacteria
+from damage
+just long enough for it
+to pass into the intestines.
+Salmonella then faces the next gauntlet,
+as intestinal cells swiftly unleash
+microbe-destroying immune cells.
+But once again, the bacteria detect
+these changes with inbuilt sensors.
+And embedded within Salmonella’s
+genome are pathogenicity islands,
+clusters of adaptive genes that
+launch the next phase of attack.
+They signal the construction
+of a specialized system
+that resembles a needle and syringe.
+Within seconds, it injects molecules
+called effector proteins
+into the intestinal cells,
+causing them to change their structure
+and swallow up the Salmonella.
+Once inside, Salmonella can then
+exploit the cell machinery
+to replicate and spread.
+But these invaded intestinal cells
+don’t go down without a fight—
+as soon as this breach begins,
+they release cytokines,
+chemical messengers that prompt
+the immune system to launch into action.
+Fleets of white blood cells seek
+out and destroy Salmonella microbes
+and infected cells.
+This inflammatory response is also
+what leads to symptoms
+like abdominal pains and fever.
+And it further damages
+the breached intestinal cells,
+limiting their usual ability
+to absorb water.
+So, whatever’s in the digestive tract
+gets released in watery diarrhea.
+While this inflammatory response
+may feel unpleasant,
+it effectively purges Salmonella from the
+body within 2 to 7 days for most people,
+without the need for antibiotics.
+But there are times when a Salmonella
+infection may require more treatment.
+It can lead to severe dehydration,
+particularly in children
+and older patients.
+And in some unusual cases, Salmonella
+can continue to spread through the body,
+hiding inside immune cells,
+invading other organs and tissues,
+and even poisoning the blood.
+These cases occur if a person is infected
+by a rare but powerful type of Salmonella
+called S. typhi.
+Unlike other strains,
+S. typhi doesn’t infect chickens—
+It spreads from person to person,
+mainly via poor sanitation
+and untreated drinking water.
+Although it’s uncommon in many parts
+of the world, typhoid fever,
+the disease that S. typhi causes,
+still kills over 100,000 people yearly.
+Thankfully, there are vaccines
+to prevent infection by S. typhi.
+To avoid milder variants,
+there are steps that everyone can take,
+like washing your hands,
+avoiding unpasteurized milk,
+and cooking meat and eggs thoroughly.
+Cookie dough should be off limits:
+raw eggs and flour both
+carry a Salmonella risk.
+And there are ways to stop
+Salmonella at its source.
+Investigations into the outbreak in 2010
+revealed one company's dark history
+of unhygienic farming conditions,
+bribery of health officials,
+and mislabeled eggs.
+Since then,
+the United States has taken steps
+to put stricter regulations in place.
+In Europe, many countries have
+successfully reduced Salmonella
+by requiring testing on farms
+and before products reach shelves.
+But there's still work to be done
+if we want to stop the spread
+of this incredibly crafty pathogen.
+  
+  `,
+  contentVi: `
+
+Translator: Nguyen Dang Khoi
+Reviewer: Nhat Minh Nguyen Ha
+Ở một trang trại nào đó tại Iowa vào năm 2010,
+một con gà mái đẻ trứng.
+Chỉ trong vài tuần ngắn ngủi,
+quả trứng này sẽ trở thành tâm điểm
+của một đợt bùng phát nhiễm khuẩn lớn:
+hàng nghìn người ngã bệnh,
+hàng triệu quả trứng bị thu hồi,
+và một loạt ông lớn trong ngành công nghiệp trứng
+cuối cùng vướng vòng lao lý,
+tất cả chỉ vì một loại vi khuẩn nhỏ bé nhưng đầy quyền lực.
+Mỗi năm, Salmonella lây nhiễm cho
+hàng triệu người trên khắp thế giới,
+gây sốt, đau quặn bụng và tiêu chảy.
+Và hậu quả có thể vô cùng nghiêm trọng:
+Salmonella là nguyên nhân hàng đầu
+gây nhập viện và tử vong
+do ngộ độc thực phẩm.
+Vậy, hãy theo dấu loài vi khuẩn này
+để xem nó gây bệnh ra sao.
+Ta bắt đầu từ đường ruột gà,
+nguồn gốc chính của Salmonella.
+Ở gà, vi khuẩn này thường
+không bị phát hiện,
+tạo điều kiện lây sang trứng
+qua lòng đỏ đang phát triển
+hoặc qua phân,
+vốn có thể làm bẩn vỏ trứng.
+Trong điều kiện chăn nuôi kém sạch,
+phân chứa Salmonella cũng có thể lây sang
+vật nuôi và cây trồng,
+gây nhiều đợt bùng phát
+liên quan đến thực phẩm.
+Trong lúc đó, thịt gà có thể nhiễm
+Salmonella từ ruột khi chế biến.
+Trên hành trình từ nông trại
+đến bàn ăn,
+vi khuẩn này có thể sống sót
+trong lạnh, ẩm hay khô hạn.
+Nhưng khi vào cơ thể người,
+Salmonella mới bộc lộ rõ
+khả năng sinh tồn.
+Rào cản đầu tiên chính là dạ dày.
+Tại đây, hầu hết vi khuẩn xâm nhập
+đều bị tiêu diệt bởi axit dạ dày.
+Nhưng tế bào Salmonella
+có thể nhận ra môi trường axit,
+kích hoạt việc tạo ra
+các protein chống sốc axit.
+Những phân tử này giúp vi khuẩn
+tránh tổn hại,
+vừa đủ thời gian để nó
+lọt vào ruột.
+Tại đây, Salmonella đối mặt
+thử thách tiếp theo,
+khi tế bào ruột lập tức gọi đến
+các tế bào miễn dịch diệt khuẩn.
+Nhưng vi khuẩn lại phát hiện những
+thay đổi nhờ bộ cảm ứng sẵn có bên trong.
+Trong bộ gen của Salmonella
+có những “đảo gây bệnh”,
+cụm gen thích nghi mở màn
+cho giai đoạn tấn công mới.
+Chúng ra tín hiệu xây dựng
+một hệ thống đặc biệt
+giống như kim tiêm và ống chích.
+Trong vài giây, nó bơm
+các protein hiệu ứng
+vào các tế bào ruột,
+làm đổi cấu trúc của chúng
+và khiến chúng nuốt Salmonella vào trong.
+Một khi đã lọt vào, Salmonella lợi dụng
+bộ máy của tế bào
+để nhân lên và lan rộng.
+Nhưng những tế bào ruột bị xâm nhập
+không hề khuất phục dễ dàng—
+ngay khi sự xâm lấn bắt đầu,
+chúng giải phóng cytokine,
+tín hiệu hóa học kêu gọi
+hệ miễn dịch phản công.
+Các đội quân bạch cầu tìm đến
+tiêu diệt vi khuẩn Salmonella
+và cả những tế bào đã nhiễm.
+Chính phản ứng viêm này cũng gây ra
+các triệu chứng như
+đau bụng và sốt.
+Nó còn làm tổn hại thêm
+các tế bào ruột bị phá vỡ,
+khiến chúng mất khả năng
+trong việc hấp thu nước.
+Vậy nên, mọi thứ trong ruột
+sẽ bị tống ra dưới dạng tiêu chảy.
+Dù phản ứng viêm này gây khó chịu,
+nhưng nó giúp quét sạch Salmonella khỏi
+cơ thể trong 2–7 ngày với đa số người,
+mà không cần đến kháng sinh.
+Tuy nhiên, đôi khi nhiễm Salmonella
+lại cần điều trị nhiều hơn.
+Nó có thể gây mất nước nghiêm trọng,
+nhất là ở trẻ nhỏ và người già.
+Trong vài trường hợp hiếm,
+Salmonella có thể lan khắp cơ thể,
+ẩn trong tế bào miễn dịch,
+xâm nhập cơ quan và mô khác,
+thậm chí còn gây nhiễm độc máu.
+Những ca này xảy ra khi người bệnh
+nhiễm phải một chủng hiếm nhưng mạnh,
+gọi là S. typhi.
+Không giống các chủng khác,
+S. typhi không lây qua gà —
+mà lây từ người sang người,
+chủ yếu do vệ sinh kém
+và nguồn nước uống không được xử lý.
+Dù hiếm gặp ở nhiều nơi,
+nhưng bệnh thương hàn
+do S. typhi vẫn cướp đi sinh mạng
+hơn 100.000 người mỗi năm.
+May mắn là đã có vắc xin
+ngăn ngừa S. typhi.
+Để tránh các biến thể nhẹ hơn,
+ai cũng có thể chủ động phòng tránh,
+như rửa tay sạch,
+tránh sữa chưa tiệt trùng,
+và nấu chín kỹ thịt cùng trứng.
+Bột bánh quy sống nên tránh xa:
+cả trứng sống và bột mì
+đều tiềm ẩn nguy cơ Salmonella.
+Vẫn có cách chặn Salmonella
+ngay từ gốc rễ.
+Cuộc điều tra vụ bùng phát 2010
+phơi bày lịch sử đen tối của một công ty:
+chăn nuôi mất vệ sinh,
+hối lộ cán bộ y tế và dán nhãn sai trứng.
+Từ đó,
+Hoa Kỳ đã siết chặt
+các quy định quản lý hơn.
+Ở châu Âu, nhiều nước
+giảm thành công Salmonella
+nhờ yêu cầu xét nghiệm ngay tại trang trại
+và trước khi hàng lên kệ.
+Nhưng vẫn còn nhiều việc phải làm
+nếu muốn chặn đứng sự lây lan
+của loài mầm bệnh tinh ranh này.
+ 
+  `,
+};
