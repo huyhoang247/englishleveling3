b8753e3f0ac775431ba574965de7f96f5d331fec
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-11-14 10:12:05 +0700

    Update VerticalFlashcardGallery.tsx

--- src/VerticalFlashcardGallery.tsx
+++ src/VerticalFlashcardGallery.tsx
@@ -50,7 +50,7 @@ const animations = `
 const HomeIcon = ({ className = "h-6 w-6" }: { className?: string }) => ( <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLineCap="round" strokeLineJoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg> );
 const BackIcon = ({ className = "h-6 w-6" }: { className?: string }) => ( <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLineCap="round" strokeLineJoin="round" d="M15 19l-7-7 7-7" /></svg> );
 const SettingsIcon = ({ className = "h-6 w-6" }: { className?: string }) => ( <img src="https://raw.githubusercontent.com/huyhoang247/englishleveling3/refs/heads/main/src/assets/images/settings.webp" alt="Settings Icon" className={className} /> );
-const MenuIcon = ({ className = "h-6 w-6" }: { className?: string }) => ( <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLineCap="round" strokeLineJoin="round" d="M4 6h16M4 12h16M4 18h16" /></svg>);
+const MenuIcon = ({ className = "h-6 w-6" }: { className?: string }) => ( <img src="https://raw.githubusercontent.com/huyhoang247/englishleveling3/refs/heads/main/src/assets/images/menu-icon-2.webp" alt="Menu Icon" className={className} /> );
 // --- END: ICONS ---
 
 
