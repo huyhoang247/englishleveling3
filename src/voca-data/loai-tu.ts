75529d5d7e7d1ddb5a5fe8acc2ddddeadeed2ab6
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-11-26 22:15:41 +0700

    Update loai-tu.ts

--- src/voca-data/loai-tu.ts
+++ src/voca-data/loai-tu.ts
@@ -4,7 +4,7 @@
 const rawData = `
 Source - Danh Từ
 Insurance - Danh Từ
-College - Danh Từ 
+College - Danh Từ / Động Từ
 Argument - Danh Từ / Tính Từ
 Influence - Danh Từ / Động Từ
 Release - Danh Từ / Động Từ
