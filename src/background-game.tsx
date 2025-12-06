e76bd465f78bc0063f096ee68850cc33d20a0ce7
huyhoang247 <ngohuyhoang.k23@gmail.com>
2025-12-06 20:59:38 +0700

    Update background-game.tsx

--- src/background-game.tsx
+++ src/background-game.tsx
@@ -10,7 +10,7 @@ import useSessionStorage from './bo-nho-tam.tsx';
 import HeaderBackground from './header-background.tsx';
 import { SidebarLayout } from './sidebar.tsx';
 import EnhancedLeaderboard from './rank.tsx';
-import PvpArena from './home/PvP/pvp-home.tsx';
+import PvpArena from './home/run-game.tsx';
 import DungeonCanvasBackground from './background-canvas.tsx';
 import LuckyChestGame from './lucky-game.tsx';
 import { uiAssets, lottieAssets } from './game-assets.ts';
