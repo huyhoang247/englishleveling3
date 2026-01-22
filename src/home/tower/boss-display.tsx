fdbe8e5467164190ff64d3105cc68f6818ef3512
huyhoang247 <ngohuyhoang.k23@gmail.com>
2026-01-22 13:49:16 +0700

    Update boss-display.tsx

--- src/home/tower/boss-display.tsx
+++ src/home/tower/boss-display.tsx
@@ -145,8 +145,8 @@ export const HeroDisplay = memo(({ stats, onStatsClick, actionState = 'idle' }: 
                 className="relative cursor-pointer group flex flex-col items-center -translate-x-4 md:-translate-x-10"
                 onClick={onStatsClick}
             >
-                {/* HP Bar */}
-                <div className="w-32 md:w-48 z-20 translate-y-2 md:translate-y-6 translate-x-2 transition-transform duration-200 group-hover:scale-105">
+                {/* HP Bar - Đã điều chỉnh translate-y để dịch lên trên */}
+                <div className="w-32 md:w-48 z-20 -translate-y-6 md:-translate-y-10 translate-x-2 transition-transform duration-200 group-hover:scale-105">
                      <HealthBar 
                         current={stats.hp} 
                         max={stats.maxHp} 
