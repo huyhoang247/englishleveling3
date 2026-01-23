cfcf4daba7b2225aa2ca90608bcc60ea5433f736
huyhoang247 <ngohuyhoang.k23@gmail.com>
2026-01-23 10:04:36 +0700

    Update tower-data.ts

--- src/home/tower/tower-data.ts
+++ src/home/tower/tower-data.ts
@@ -42,7 +42,7 @@ const BOSS_DATA: Boss[] = [
     { id: 21, floor: "FLOOR 21", name: "Scarlet Eyeball", stats: { maxHp: 3834, hp: 3834, atk: 383, def: 230 }, rewards: { coins: 3451, energy: 9 } },
     { id: 22, floor: "FLOOR 22", name: "Ninefathom", stats: { maxHp: 4601, hp: 4601, atk: 460, def: 276 }, rewards: { coins: 4141, energy: 9 } },
     { id: 23, floor: "FLOOR 23", name: "Wailing Child", stats: { maxHp: 5521, hp: 5521, atk: 552, def: 331 }, rewards: { coins: 4969, energy: 9 } },
-    { id: 24, floor: "FLOOR 24", name: "Thorned Creeper", stats: { maxHp: 6626, hp: 6626, atk: 663, def: 398 }, rewards: { coins: 5963, energy: 9 } },
+    { id: 24, floor: "FLOOR 24", name: "Shardwisp", stats: { maxHp: 6626, hp: 6626, atk: 663, def: 398 }, rewards: { coins: 5963, energy: 9 } },
     { id: 25, floor: "FLOOR 25", name: "Fiendish Bat", stats: { maxHp: 7951, hp: 7951, atk: 795, def: 477 }, rewards: { coins: 7156, energy: 10 } },
     { id: 26, floor: "FLOOR 26", name: "Gnashing Maw", stats: { maxHp: 9541, hp: 9541, atk: 954, def: 572 }, rewards: { coins: 8587, energy: 10 } },
     { id: 27, floor: "FLOOR 27", name: "Moldering Husk", stats: { maxHp: 11449, hp: 11449, atk: 1145, def: 687 }, rewards: { coins: 10304, energy: 10 } },
